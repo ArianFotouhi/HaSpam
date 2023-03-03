4c7f13467784df657ee5755b4d535c2ef4467afe
Arian <78182876+ArianFotouhi@users.noreply.github.com>
2023-03-03 13:38:52 -0500

    v1.3

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
-from sklearn.model_selection import train_test_split
+
+
+
 
 df = pd.read_csv('./TextFiles/smsspamcollection.tsv',sep='\t')
 df.head()
@@ -15,6 +16,8 @@ print('Categories: ',df['label'].unique())
 #showing the number of each labels
 print('No. of each category: ',df['label'].value_counts())
 
+#visualization
+import matplotlib.pyplot as plt
 #visualization of text label based on text length
 plt.xscale('log')
 bins = 1.15**(np.arange(0,50))
@@ -35,3 +38,28 @@ plt.show()
 #result: a small tendency of spam towards more punctutation (not a firm inference)
 
 #Data Preprocessing
+
+#features
+X = df[['length','punct']]
+
+#labels
+y = df['label']
+
+#data split
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X,y,test_size=0.3,random_state=0)
+
+#shape of train and test data
+print('train data shape:', X_train.shape)
+print('test data shape:', X_test.shape)
+
+#ML model 
+from sklearn.linear_model import LogisticRegression
+lr_model = LogisticRegression(solver='lbfgs')
+
+lr_model.fit(X_train,y_train)
+
+#Accuracy
+from sklearn import metrics
+predictions = lr_model.predict(X_test)
+
